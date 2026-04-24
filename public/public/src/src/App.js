ff557e042fe5433a377860ef851243809480448f
emeaexecutive <emeaexecutive@icloud.com>
2026-04-24 10:38:33 +0200

    Add App component with CyberSentinelsDashboardV1

---
+++ public/public/src/src/App.js
@@ -0,0 +1,12 @@
+import CyberSentinelsDashboardV1 from './components/CyberSentinelsDashboardV1';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CyberSentinelsDashboardV1 />
+    </div>
+  );
+}
+
+export default App;
